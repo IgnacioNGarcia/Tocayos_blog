b0fa9076c9a7a5cbe8439852590f0f875da73a0b
Ignacio Daniel García <igniciagar@gmail.com>
2023-11-01 21:33:49 -0300

    acomodo html github, falta esconder los tokens

--- github.js
+++ github.js
@@ -1,69 +1,87 @@
-function fetchGitHubData(username, userId) {
-    // Concatenar el número de usuario con el ID
+async function fetchGitHubData(username, userId, token) {
     const nombreId = `Nombre${userId}`;
     const usuarioId = `Usuario${userId}`;
     const bioId = `Bio${userId}`;
     const profileImageId = `profileImage${userId}`;
   
-    // Fetch y muestra la información del perfil de GitHub
-    fetch(`https://api.github.com/users/${username}`)
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById(nombreId).textContent = data.name;
-        document.getElementById(usuarioId).textContent = data.login;
-        document.getElementById(bioId).textContent = data.bio;
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`, {
+        headers: {
+          'Authorization': `token ${token}`
+        }
+      });
+      if (response.status !== 200) {
+        throw new Error(`Failed to fetch user data for ${username}`);
+      }
+      const data = await response.json();
   
-        const profileImage = document.getElementById(profileImageId);
-        profileImage.src = data.avatar_url;
-        profileImage.alt = `GitHub Profile Picture - ${data.login}`;
-      })
-      .catch(error => console.error('Error:', error));
+      document.getElementById(nombreId).textContent = data.name;
+      document.getElementById(usuarioId).textContent = data.login;
+      document.getElementById(bioId).textContent = data.bio;
   
-    // Fetch y muestra la información de los repositorios
-    fetch(`https://api.github.com/users/${username}/repos`)
-      .then(response => response.json())
-      .then(repositories => {
-        repositories.forEach(repository => {
-          const repositoryName = repository.name;
-          const repositoryDescription = repository.description;
-          const repositoryOwner = repository.owner.login;
-          const repoId = `repositoriesContainer${userId}`;
-          fetch(`https://api.github.com/repos/${username}/${repositoryName}/commits`)
-            .then(response => response.json())
-            .then(commits => {
-              const commitCount = commits.length;
+      const profileImage = document.getElementById(profileImageId);
+      profileImage.src = data.avatar_url;
+      profileImage.alt = `GitHub Profile Picture - ${data.login}`;
+    } catch (error) {
+      console.error('Error:', error);
+      return;
+    }
   
-              const repositoryInfo = document.createElement('div');
-              repositoryInfo.innerHTML = `
-                <h3>${repositoryName}</h3>
-                <p>Descripción: ${repositoryDescription}</p>
-                <p>Propietario: ${repositoryOwner}</p>
-                <p>Commits: ${commitCount}</p>
-              `;
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}/repos`, {
+        headers: {
+          'Authorization': `token ${token}`
+        }
+      });
+      if (response.status !== 200) {
+        throw new Error(`Failed to fetch user repositories for ${username}`);
+      }
+      const repositories = await response.json();
   
-              document.getElementById(repoId).appendChild(repositoryInfo);
-            })
-            .catch(error => console.error('Error:', error));
-        });
-      })
-      .catch(error => console.error('Error:', error));
+      repositories.forEach(async (repository) => {
+        const repositoryName = repository.name;
+        const repositoryDescription = repository.description;
+        const repositoryOwner = repository.owner.login;
+        const repoId = `repositoriesContainer${userId}`;
+  
+        try {
+          const commitsResponse = await fetch(
+            `https://api.github.com/repos/${username}/${repositoryName}/commits`, {
+              headers: {
+                'Authorization': `token ${token}`
+              }
+            });
+          if (commitsResponse.status !== 200) {
+            throw new Error(`Failed to fetch commits for ${repositoryName}`);
+          }
+          const commits = await commitsResponse.json();
+  
+          const commitCount = commits.length;
+  
+          const repositoryInfo = document.createElement('div');
+          repositoryInfo.innerHTML = `
+            <h3>${repositoryName}</h3>
+            <p>Descripción: ${repositoryDescription}</p>
+            <p>Propietario: ${repositoryOwner}</p>
+            <p>Commits: ${commitCount}</p>
+          `;
+  
+          document.getElementById(repoId).appendChild(repositoryInfo);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
   
-  // Llama a la función con el nombre de usuario deseado
-  fetchGitHubData('ignacioDanGarcia',1);
-  fetchGitHubData('ignacioNGarcia',2);
-
-    /*
-
-fetch('https://api.github.com/users/ignacioNGarcia')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('Nombre2').textContent = data.name;
-            document.getElementById('Usuario2').textContent = data.login;
-            document.getElementById('Bio2').textContent = data.bio;
-
-            const profileImage = document.getElementById('profileImage2');
-            profileImage.src = data.avatar_url;
-            profileImage.alt = `GitHub Profile Picture - ${data.login}`;
-        })
-        .catch(error => console.error('Error:', error));*/
+  const users = [
+    { username: 'ignacioDanGarcia', userId: 1 , token: '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'},
+    { username: 'ignacioNGarcia', userId: 2 , token: ''},
+  ];
+  
+  users.forEach((user) => {
+    fetchGitHubData(user.username, user.userId, user.token);
+  });
+  
